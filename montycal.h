d1e8bd37605031c563dbe4d77fe3a311ee26f13b
CletsyMedia <Cletussam12@yahoo.com>
2023-10-15 05:21:58 +0100

    Montycal scroll

--- montycal.h
+++ montycal.h
@@ -2,23 +2,12 @@
 #define _MONTYCAL_H
 #define _POSIX_C_SOURCE 200809L
 
-#include <unistd.h>
-#include <stdlib.h>
-#include <sys/types.h>
-#include <ctype.h>
-#include <stdio.h>
-#include <fcntl.h>
-#include <string.h>
-
-
-
-
-
-
-
-
-
-
-
+#include <unistd.h>   /* Provides access to POSIX operating system APIs */
+#include <stdlib.h>   /* Contains declarations for general-purpose functions */
+#include <sys/types.h>/* Defines various data types */
+#include <ctype.h>    /* Provides character classification functions */
+#include <stdio.h>    /* Used for input and output operations */
+#include <fcntl.h>    /* Provides file control options */
+#include <string.h>   /* Declares functions for string manipulation */
 
 #endif /* _MONTYCAL_H */
