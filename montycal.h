24fc410b7b6dc32c4f3087bde0fcf5d6847e709c
CletsyMedia <Cletussam12@yahoo.com>
2023-10-15 05:18:09 +0100

    Montycal scroll

---
+++ montycal.h
@@ -0,0 +1,24 @@
+#ifndef _MONTYCAL_H
+#define _MONTYCAL_H
+#define _POSIX_C_SOURCE 200809L
+
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <ctype.h>
+#include <stdio.h>
+#include <fcntl.h>
+#include <string.h>
+
+
+
+
+
+
+
+
+
+
+
+
+#endif /* _MONTYCAL_H */
