ea7a5323e0e8bb1e6bfc5998e2215917f15e21f2
CletsyMedia <Cletussam12@yahoo.com>
2023-10-17 23:48:55 +0100

    Node and queues for monty

--- node_Queues.c
+++ node_Queues.c
@@ -36,3 +36,60 @@ void _adding_node(stack_t **head, int n)
 	/* Update the head to point to the new node (new top) */
 	*head = newNode;
 }
+
+/**
+ * _queuing_f - Set the queue mode.
+ * @head: Pointer to the stack's head
+ * @count: Line number
+ *
+ * Description:
+ * This function sets the queue mode, which changes the behavior of the push
+ * operation to add elements to the back of the stack instead of the front.
+ *
+ * Return: No return value
+ */
+void _queuing_f(stack_t **head, unsigned int count)
+{
+	(void)head;
+	(void)count;
+	bus.lifi = 1;
+}
+/**
+ * _adding_queue - Adds a new node to the end of the stack.
+ * @head: Pointer to the stack's head
+ * @n: Value to be stored in the new node
+ *
+ * Description:
+ * This function creates a new node with the given value and adds it to the end
+ * of the stack. If the allocation of memory for the new node fails, it prints
+ * an error message.
+ *
+ * Return: No return value
+ */
+void _adding_queue(stack_t **head, int n)
+{
+	stack_t *newNode, *auxil;
+
+	newNode = malloc(sizeof(stack_t));
+	if (!newNode)
+	{
+	fprintf(stderr, "Error: Can't malloc\n");
+	exit(EXIT_FAILURE);
+	}
+	newNode->n = n;
+	newNode->next = NULL;
+
+	if (!*head)
+	{
+	newNode->prev = NULL;
+	*head = newNode;
+	return;
+	}
+
+	auxil = *head;
+	while (auxil->next)
+	auxil = auxil->next;
+
+	newNode->prev = auxil;
+	auxil->next = newNode;
+}
