ca8079bf8b83ebe917cc6aacafd3222258f99706
CletsyMedia <Cletussam12@yahoo.com>
2023-10-17 23:37:49 +0100

    Monty default Outputs

--- montyOutput.c
+++ montyOutput.c
@@ -14,38 +14,37 @@
  */
 void _pchar_f(stack_t **head, unsigned int count)
 {
-	stack_t *h;
+  stack_t *h;
 
-	h = *head;
+  h = *head;
 
-	if (!h)
-	{
-	fprintf(stderr, "L%d: Error: Char stack is empty\n", count);
-	fclose(bus.file);
+  if (!h)
+  {
+    fprintf(stderr, "L%d: Error: Char stack is empty\n", count);
+    fclose(bus.file);
 
-	free(bus.cont);
+    free(bus.cont);
 
-	freeing_stack(*head);
+    freeing_stack(*head);
 
-	exit(EXIT_FAILURE);
-	}
+    exit(EXIT_FAILURE);
+  }
 
-	if (h->n > 127 || h->n < 0)
-	{
-	fprintf(stderr, "L%d: Error: Char value is out of range\n", count);
+  if (h->n > 127 || h->n < 0)
+  {
+    fprintf(stderr, "L%d: Error: Char value is out of range\n", count);
 
-	fclose(bus.file);
+    fclose(bus.file);
 
-	free(bus.cont);
+    free(bus.cont);
 
-	freeing_stack(*head);
+    freeing_stack(*head);
 
-	exit(EXIT_FAILURE);
-	}
+    exit(EXIT_FAILURE);
+  }
 
-	printf("%c\n", h->n);
+  printf("%c\n", h->n);
 }
-
 
 /**
  * _pstring_f - Prints a string of characters from the stack.
@@ -62,17 +61,16 @@ void _pchar_f(stack_t **head, unsigned int count)
  */
 void _pstring_f(stack_t **head, unsigned int count)
 {
-	stack_t *h;
-	(void)count;
+  stack_t *h;
+  (void)count;
 
-	for (h = *head; h && (h->n != 0 && h->n <= 127 && h->n > 0); h = h->next)
-	{
-	printf("%c", h->n);
-	}
+  for (h = *head; h && (h->n != 0 && h->n <= 127 && h->n > 0); h = h->next)
+  {
+    printf("%c", h->n);
+  }
 
-	printf("\n");
+  printf("\n");
 }
-
 
 /**
  * _rotateT_f - Rotate the stack to the top.
@@ -87,23 +85,53 @@ void _pstring_f(stack_t **head, unsigned int count)
  */
 void _rotateT_f(stack_t **head, __attribute__((unused)) unsigned int count)
 {
-	if (*head == NULL || (*head)->next == NULL)
-	{
-	return;
-	}
+  if (*head == NULL || (*head)->next == NULL)
+  {
+    return;
+  }
 
-	stack_t *cpy = *head;
+  stack_t *cpy = *head;
 
-	while (cpy->next)
-	{
-	cpy = cpy->next;
-	}
+  while (cpy->next)
+  {
+    cpy = cpy->next;
+  }
 
-	cpy->next = *head;
-	cpy->prev->next = NULL;
-	cpy->prev = NULL;
-	(*head)->prev = cpy;
-	(*head) = cpy;
+  cpy->next = *head;
+  cpy->prev->next = NULL;
+  cpy->prev = NULL;
+  (*head)->prev = cpy;
+  (*head) = cpy;
 }
 
+/**
+ * _rotateB_f - Rotate the stack to the bottom.
+ * @head: Pointer to the stack's head
+ * @count: Line number (unused)
+ *
+ * Description:
+ * This function rotates the stack to the bottom, moving the top element
+ * to the bottom of the stack.
+ *
+ * Return: No return value
+ */
+void _rotateB_f(stack_t **head, __attribute__((unused)) unsigned int count)
+{
+  if (*head == NULL || (*head)->next == NULL)
+  {
+    return;
+  }
 
+  stack_t *tmp = *head;
+  stack_t *auxil = (*head)->next;
+
+  while (tmp->next != NULL)
+  {
+    tmp = tmp->next;
+  }
+
+  tmp->next = *head;
+  (*head)->next = NULL;
+  (*head)->prev = tmp;
+  (*head) = auxil;
+}
