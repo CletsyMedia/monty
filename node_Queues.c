65d491b3820dd5002aabd70c73aebafb06b8d6b8
CletsyMedia <Cletussam12@yahoo.com>
2023-10-17 23:47:11 +0100

    Node and queues for monty

---
+++ node_Queues.c
@@ -0,0 +1,38 @@
+#include "montycal.h"
+
+
+/**
+ * _adding_node - Adds a new node to the stack.
+ * @head: Pointer to the stack's head
+ * @n: Value to be stored in the new node
+ *
+ * Description:
+ * This function creates a new node, initializes it with the given value, and
+ * adds it to the top of the stack. If the allocation of memory for the new
+ * node fails, it prints an error message and exits the program.
+ * Returns: no return value
+ */
+void _adding_node(stack_t **head, int n)
+{
+	stack_t *newNode; /* Declare a pointer to the new node */
+	stack_t *auxil;		/* Declare a pointer to the current top of the stack */
+	/* Set auxil to point to the current top of the stack */
+	auxil = *head;
+	newNode = malloc(sizeof(stack_t)); /* Allocate memory for the new node */
+
+	if (newNode == NULL) /* Check if memory allocation failed */
+	{
+		printf("Error: Can't malloc\n");
+		exit(0);
+	}
+
+	if (auxil)							 /* Check if the stack is not empty */
+		auxil->prev = newNode; /* Update the previous node's 'prev' pointer */
+
+	newNode->n = n; /* Set the value of the new node to 'n' */
+	/* Set the new node's 'next' to point to the current top */
+	newNode->next = *head;
+	newNode->prev = NULL; /* Set the new node's 'prev' to NULL */
+	/* Update the head to point to the new node (new top) */
+	*head = newNode;
+}
