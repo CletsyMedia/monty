919f7bd3c3f9218a1607de55064894bc9c46fb8d
CletsyMedia <Cletussam12@yahoo.com>
2023-10-18 19:44:46 +0100

    Handling monty files

--- file_handle.c
+++ file_handle.c
@@ -8,13 +8,13 @@
 
 void opening_file(char *file_name)
 {
-  FILE *fd = fopen(file_name, "r");
+	FILE *fd = fopen(file_name, "r");
 
-  if (file_name == NULL || fd == NULL)
-    error(2, file_name);
+	if (file_name == NULL || fd == NULL)
+	error(2, file_name);
 
-  reading_file(fd);
-  fclose(fd);
+	reading_file(fd);
+	fclose(fd);
 }
 
 /**
@@ -25,16 +25,16 @@ void opening_file(char *file_name)
 
 void reading_file(FILE *fd)
 {
-  int format = 0;
-  char *buffs = NULL;
-  size_t length = 0;
-  int line_num;
+	int format = 0;
+	char *buffs = NULL;
+	size_t length = 0;
+	int line_num;
 
-  for (line_num = 1; getline(&buffs, &length, fd) != -1; line_num++)
-  {
-    format = parsing_line(buffs, line_num, format);
-  }
-  free(buffs);
+	for (line_num = 1; getline(&buffs, &length, fd) != -1; line_num++)
+	{
+	format = parsing_line(buffs, line_num, format);
+	}
+	free(buffs);
 }
 
 /**
@@ -42,7 +42,7 @@ void reading_file(FILE *fd)
  * which function to call
  * @buffer: line from the file
  * @line_number: line number
- * @format:  storage format. If 0 Nodes will be entered as a stack.
+ * @format:	storage format. If 0 Nodes will be entered as a stack.
  * if 1 nodes will be entered as a queue.
  * Return: Returns 0 if the opcode is stack. 1 if queue.
  */
@@ -50,28 +50,28 @@ void reading_file(FILE *fd)
 int parsing_line(char *buffer, int line_number, int format)
 {
 	char *opcode;
-  char *val;
+	char *val;
 	const char *delim = "\n ";
 
 	if (buffer == NULL)
-		error(4);
+	error(4);
 
 	opcode = strtok(buffer, delim);
 	if (opcode == NULL)
-		return (format);
+	return (format);
 	val = strtok(NULL, delim);
 
 	if (strcmp(opcode, "stack") == 0)
-		return (0);
+	return (0);
 	if (strcmp(opcode, "queue") == 0)
-		return (1);
+	return (1);
 
 	finding_func(opcode, val, line_number, format);
 	return (format);
 }
 
 /**
- * find_func - find the appropriate function for the opcode
+ * finding_func - find the appropriate function for the opcode
  * @opcode: opcode
  * @value: argument of opcode
  * @format:  storage format. If 0 Nodes will be entered as a stack.
@@ -79,29 +79,28 @@ int parsing_line(char *buffer, int line_number, int format)
  * if 1 nodes will be entered as a queue.
  * Return: void
  */
-void find_func(char *opcode, char *value, int ln, int format)
+void finding_func(char *opcode, char *value, int ln, int format)
 {
 	int a, flaging;
 
 	instruction_t func_list[] = {
-    {"push", adding_to_stack},
-    {"pall", printing_stack},
-    {"pint", prnt_top},
-    {"pop", poping},
-    {"nop", no_op},
-    {"swap", swapping},
-    {"add", addition},
-    {"sub", subtract},
-    {"div", divide},
-    {"mul", multiply},
-    {"mod", modulus},
-    {"pchar", prnt_char},
-    {"pstr", prnt_str},
-    {"rotl", rotl},
-    {"rotr", rotr},
-    {NULL, NULL}
-};
-
+		{"push", adding_to_stack},
+		{"pall", printing_stack},
+		{"pint", prnt_top},
+		{"pop", poping},
+		{"nop", no_op},
+		{"swap", swapping},
+		{"add", addition},
+		{"sub", subtract},
+		{"div", divide},
+		{"mul", multiply},
+		{"mod", modulus},
+		{"pchar", prnt_char},
+		{"pstr", prnt_str},
+		{"rotl", rotl},
+		{"rotr", rotr},
+		{NULL, NULL}
+	};
 
 	if (opcode[0] == '#')
 		return;
@@ -116,4 +115,43 @@ void find_func(char *opcode, char *value, int ln, int format)
 	}
 	if (flaging == 1)
 		error(3, ln, opcode);
+}
+
+/**
+ * calling_func - Calls the required function.
+ * @func: Pointer to the function that is about to be called.
+ * @op: string representing the opcode.
+ * @val: string representing a numeric value.
+ * @ln: line numeber for the instruction.
+ * @format: Format specifier. If 0 Nodes will be entered as a stack.
+ * if 1 nodes will be entered as a queue.
+ */
+void calling_func(op_func func, char *op, char *val, int ln, int format)
+{
+	stack_t *node;
+	int flaging, a;
+
+	flaging = 1;
+	if (strcmp(op, "push") == 0)
+	{
+	if (val != NULL && val[0] == '-')
+	{
+	val = val + 1;
+	flaging = -1;
+	}
+	if (val == NULL)
+	error(5, ln);
+	for (a = 0; val[a] != '\0'; a++)
+	{
+	if (isdigit(val[a]) == 0)
+	error(5, ln);
+	}
+	node = creating_node(atoi(val) * flaging);
+	if (format == 0)
+	func(&node, ln);
+	if (format == 1)
+	adding_to_queue(&node, ln);
+	}
+	else
+	func(&head, ln);
 }
