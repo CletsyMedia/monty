e5534956fda0f0f09ad8a802b687a10592868228
CletsyMedia <Cletussam12@yahoo.com>
2023-10-18 19:58:39 +0100

    Monty main

---
+++ monty.c
@@ -0,0 +1,22 @@
+#include "montycal.h"
+stack_t *head = NULL;
+
+/**
+ * main - entry point
+ * @argc: arguments count
+ * @argv: list of arguments
+ * Return: always 0
+ */
+
+int main(int argc, char *argv[])
+{
+  if (argc != 2)
+  {
+    fprintf(stderr, "USAGE: monty file\n");
+    exit(EXIT_FAILURE);
+  }
+  opening_file(argv[1]);
+  freeing_nodes();
+  return (0);
+}
+
