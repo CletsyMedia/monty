057e19991120e2fa1b89bf52f63e104763ab0e80
CletsyMedia <Cletussam12@yahoo.com>
2023-10-18 18:05:10 +0100

    Handling monty files

--- file_handle.c
+++ file_handle.c
@@ -16,3 +16,23 @@ void opening_file(char *file_name)
   reading_file(fd);
   fclose(fd);
 }
+
+/**
+ * reading_file - reads a file
+ * @fd: pointer to file descriptor
+ * Return: void
+ */
+
+void reading_file(FILE *fd)
+{
+  int format = 0;
+  char *buffs = NULL;
+  size_t length = 0;
+  int line_num;
+
+  for (line_num = 1; getline(&buffs, &length, fd) != -1; line_num++)
+  {
+    format = parsing_line(buffs, line_num, format);
+  }
+  free(buffs);
+}
