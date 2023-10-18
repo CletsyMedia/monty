6128cb56d130d9c23eca7d9d5f378546b61cdcff
CletsyMedia <Cletussam12@yahoo.com>
2023-10-18 17:58:18 +0100

    Monty strings

--- strnMonty.c
+++ strnMonty.c
@@ -68,3 +68,27 @@ void rotl(stack_t **stack, __attribute__((unused)) unsigned int ln)
   (*stack)->prev->next = NULL;
   (*stack)->prev = NULL;
 }
+
+/**
+ * rotr - Rotates the last node of the stack to the top.
+ * @stack: Pointer to a pointer pointing to top node of the stack.
+ * @ln: Interger representing the line number of of the opcode.
+ */
+void rotr(stack_t **stack, __attribute__((unused)) unsigned int ln)
+{
+  stack_t *temp;
+
+  if (stack == NULL || *stack == NULL || (*stack)->next == NULL)
+    return;
+
+  temp = *stack;
+
+  while (temp->next != NULL)
+    temp = temp->next;
+
+  temp->next = *stack;
+  temp->prev->next = NULL;
+  temp->prev = NULL;
+  (*stack)->prev = temp;
+  (*stack) = temp;
+}
