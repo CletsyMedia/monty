8a915f9c2c680de0d8aa06547da5ecbd035a35d2
CletsyMedia <Cletussam12@yahoo.com>
2023-10-18 18:16:59 +0100

    Handling monty files

--- file_handle.c
+++ file_handle.c
@@ -70,3 +70,50 @@ int parsing_line(char *buffer, int line_number, int format)
 	return (format);
 }
 
+/**
+ * find_func - find the appropriate function for the opcode
+ * @opcode: opcode
+ * @value: argument of opcode
+ * @format:  storage format. If 0 Nodes will be entered as a stack.
+ * @ln: line number
+ * if 1 nodes will be entered as a queue.
+ * Return: void
+ */
+void find_func(char *opcode, char *value, int ln, int format)
+{
+	int a, flaging;
+
+	instruction_t func_list[] = {
+    {"push", adding_to_stack},
+    {"pall", printing_stack},
+    {"pint", prnt_top},
+    {"pop", poping},
+    {"nop", no_op},
+    {"swap", swapping},
+    {"add", addition},
+    {"sub", subtract},
+    {"div", divide},
+    {"mul", multiply},
+    {"mod", modulus},
+    {"pchar", prnt_char},
+    {"pstr", prnt_str},
+    {"rotl", rotl},
+    {"rotr", rotr},
+    {NULL, NULL}
+};
+
+
+	if (opcode[0] == '#')
+		return;
+
+	for (flaging = 1, a = 0; func_list[a].opcode != NULL; a++)
+	{
+		if (strcmp(opcode, func_list[a].opcode) == 0)
+		{
+			calling_func(func_list[a].f, opcode, value, ln, format);
+			flaging = 0;
+		}
+	}
+	if (flaging == 1)
+		error(3, ln, opcode);
+}
