f539e7533433d781ef1a08634333683380e3ea04
CletsyMedia <Cletussam12@yahoo.com>
2023-10-18 18:03:03 +0100

    Handling monty files

--- file_handle.c
+++ file_handle.c
@@ -8,11 +8,11 @@
 
 void opening_file(char *file_name)
 {
-	FILE *fd = fopen(file_name, "r");
+  FILE *fd = fopen(file_name, "r");
 
-	if (file_name == NULL || fd == NULL)
-		err(2, file_name);
+  if (file_name == NULL || fd == NULL)
+    error(2, file_name);
 
-	read_file(fd);
-	fclose(fd);
+  reading_file(fd);
+  fclose(fd);
 }
