33534575f2d5a8d06a1e21ab0ed1f424c91098ba
CletsyMedia <Cletussam12@yahoo.com>
2023-10-18 18:07:54 +0100

    Handling monty files

--- file_handle.c
+++ file_handle.c
@@ -36,3 +36,37 @@ void reading_file(FILE *fd)
   }
   free(buffs);
 }
+
+/**
+ * parsing_line - Separates each line into tokens to determine
+ * which function to call
+ * @buffer: line from the file
+ * @line_number: line number
+ * @format:  storage format. If 0 Nodes will be entered as a stack.
+ * if 1 nodes will be entered as a queue.
+ * Return: Returns 0 if the opcode is stack. 1 if queue.
+ */
+
+int parsing_line(char *buffer, int line_number, int format)
+{
+	char *opcode;
+  char *val;
+	const char *delim = "\n ";
+
+	if (buffer == NULL)
+		error(4);
+
+	opcode = strtok(buffer, delim);
+	if (opcode == NULL)
+		return (format);
+	val = strtok(NULL, delim);
+
+	if (strcmp(opcode, "stack") == 0)
+		return (0);
+	if (strcmp(opcode, "queue") == 0)
+		return (1);
+
+	finding_func(opcode, val, line_number, format);
+	return (format);
+}
+
