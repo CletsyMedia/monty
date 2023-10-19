315cd7a722ae9fe85eb77adacbe7ff9e4ff019a4
CletsyMedia <Cletussam12@yahoo.com>
2023-10-19 15:06:32 +0100

    Final spell on monty

---
+++ monty.c
@@ -0,0 +1,32 @@
+#include "monty.h"
+stack_t *head = NULL;
+
+/**
+ * main - Entry point for the Monty bytecode interpreter.
+ *
+ * This function serves as the entry point for the Monty bytecode interpreter.
+ * It processes command-line arguments, checks for valid input, and executes
+ * the bytecode contained in the specified file. If any errors occur during
+ * this process, they are reported to the standard error stream.
+ *
+ * @argc: The number of command-line arguments.
+ * @argv: An array of strings representing the command-line arguments.
+ *
+ * Return: Always returns 0 upon successful execution.
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc != 2)
+	{
+		fprintf(stderr, "USAGE: monty file\n");
+		exit(EXIT_FAILURE);
+	}
+	/* Open and process the specified Monty bytecode file. */
+	opening_file(argv[1]);
+
+	/* Free any allocated memory before program termination. */
+	freeing_nodes();
+
+	return (0);
+}
---
+++ strnMonty.c
@@ -0,0 +1,94 @@
+#include "monty.h"
+
+/**
+ * prnt_char - Prints the Ascii value.
+ * @stack: Pointer to a pointer pointing to top node of the stack.
+ * @line_number: Interger representing the line number of of the opcode.
+ */
+void prnt_char(stack_t **stack, unsigned int line_number)
+{
+	int ansi;
+
+	if (stack == NULL || *stack == NULL)
+	strng_error(11, line_number);
+
+	ansi = (*stack)->n;
+	if (ansi < 0 || ansi > 127)
+	strng_error(10, line_number);
+	printf("%c\n", ansi);
+}
+
+/**
+ * prnt_str - Prints a string.
+ * @stack: Pointer to a pointer pointing to top node of the stack.
+ * @ln: Interger representing the line number of of the opcode.
+ */
+void prnt_str(stack_t **stack, __attribute__((unused)) unsigned int ln)
+{
+	stack_t *temp;
+	int ansi;
+
+	if (stack == NULL || *stack == NULL)
+	{
+	printf("\n");
+	return;
+	}
+
+	temp = *stack;
+	while (temp != NULL)
+	{
+	ansi = temp->n;
+	if (ansi <= 0 || ansi > 127)
+	break;
+	printf("%c", ansi);
+	temp = temp->next;
+	}
+	printf("\n");
+}
+
+/**
+ * rotl - Rotates the first node of the stack to the bottom.
+ * @stack: Pointer to a pointer pointing to top node of the stack.
+ * @ln: Interger representing the line number of of the opcode.
+ */
+void rotl(stack_t **stack, __attribute__((unused)) unsigned int ln)
+{
+	stack_t *temp;
+
+	if (stack == NULL || *stack == NULL || (*stack)->next == NULL)
+	return;
+
+	temp = *stack;
+	while (temp->next != NULL)
+	temp = temp->next;
+
+	temp->next = *stack;
+	(*stack)->prev = temp;
+	*stack = (*stack)->next;
+	(*stack)->prev->next = NULL;
+	(*stack)->prev = NULL;
+}
+
+/**
+ * rotr - Rotates the last node of the stack to the top.
+ * @stack: Pointer to a pointer pointing to top node of the stack.
+ * @ln: Interger representing the line number of of the opcode.
+ */
+void rotr(stack_t **stack, __attribute__((unused)) unsigned int ln)
+{
+	stack_t *temp;
+
+	if (stack == NULL || *stack == NULL || (*stack)->next == NULL)
+	return;
+
+	temp = *stack;
+
+	while (temp->next != NULL)
+	temp = temp->next;
+
+	temp->next = *stack;
+	temp->prev->next = NULL;
+	temp->prev = NULL;
+	(*stack)->prev = temp;
+	(*stack) = temp;
+}
